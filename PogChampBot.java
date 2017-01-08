d23814a455c8eba5feb41a5e367a99a9fc88b343
Calvin Keats <calvin.keats@gmail.com>
2017-01-08 12:58:51 -0500

    Additional fixes
    -Now records number of PogChamps the bot does
    -Now saves number of PogChamps the bot does
    -Now loads PogChamp counters
    -Greatly reduced time between PogChamps
    -Reduced number of PogChamps needed to trigger a PogChamp
    -Added !status
    -Added Timestamp and Count to message displayed when its "Time to
    PogChamp!"

--- PogChampBot.java
+++ PogChampBot.java
@@ -1,4 +1,5 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -6,6 +7,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Map;
+import java.util.Random;
 
 import org.jibble.pircbot.PircBot;
 
@@ -19,10 +21,13 @@ class PogChampBot extends PircBot {
 	long timeSincePogChamp;
 	long timeAtPogChamp;
 	int PogChampCount;
+	int myPogChamps;
 	int counter;
 	int timeout = 1000 * 60 * 60; //1 hour
 	long startTime = 1483891200000L;
 	ArrayList<String> lastTenMessages;
+	
+	Random rand;
 	
 	PogChampBot() throws IOException {
 		this.setAuthentication();
@@ -30,24 +35,26 @@ class PogChampBot extends PircBot {
 		
 		lastTenMessages = new ArrayList<String>();
 		lastMessageTime = System.currentTimeMillis();
-		PogChampCount = 0;
+		
+		rand = new Random();
 	}
 	
 	
 	public void onMessage(String channel, String sender, String login, String hostname, String message) {
 		
 		lastMessageTime = System.currentTimeMillis();
-		System.out.println(lastMessageTime);
 		
 		
 		if (channel.equals("#krohnos")) {
-			sendMessage(channel, "Hello! Current PogChamp count is [" + PogChampCount + "]");
-			if (message.equals("!save")) {
+			if (message.equals("!status")) {
+				sendMessage(channel, "Hello! Current PogChamp count is [" + PogChampCount + "]. My count is [" + myPogChamps + "]");
+			}
+			else if (message.equals("!save")) {
 				if (writePogChampsToFile()) {
-					sendMessage(channel, "PogChamps saved! Current count is [" + PogChampCount + "]");
+					sendMessage(channel, "PogChamps saved! Current count is [" + PogChampCount + "]. My count is [" + myPogChamps + "]");
 				}
 				else {
-					sendMessage(channel, "PogChamps NOT saved WutFace Current count is [" + PogChampCount + "]");
+					sendMessage(channel, "PogChamps NOT saved WutFace Current count is [" + PogChampCount + "]. My count is [" + myPogChamps + "]");
 				}
 			}
 		}
@@ -55,7 +62,6 @@ class PogChampBot extends PircBot {
 		
 		//if AGDQ 2017 hasn't started
 		if (System.currentTimeMillis() < startTime) {
-			System.out.println("AGDQ2017 hasn't started!");
 				return;
 		}
 		
@@ -67,16 +73,20 @@ class PogChampBot extends PircBot {
 		}
 		
 		
-		if (timeAtPogChamp + 30000 < lastMessageTime && shouldWePogChamp()) {
+		if (timeAtPogChamp + 8000 < lastMessageTime && shouldWePogChamp()) {
 			timeAtPogChamp = System.currentTimeMillis();
-			sendMessage(channel, "PogChamp");
+			//System.out.println("\"PogChamp " + new String(new char[rand.nextInt(3)]).replace("\0", " ") + "\"");
+			//sendMessage(channel, "PogChamp ");
+			sendMessage(channel, "PogChamp " + new String(new char[rand.nextInt(3)]).replace("\0", " "));
 			PogChampCount++;
+			myPogChamps++;
+			System.out.println("Time to PogChamp! [Time: " + timeAtPogChamp + "] [My Count: " + myPogChamps + "]");
 		}
 		
 		if (message.contains("PogChamp")) {
 			PogChampCount += (message.split("PogChamp", -1).length-1);	
-			if (PogChampCount % 10 == 1) {
-				System.out.println("Number of messages containing PogChamp: " + PogChampCount);
+			if (PogChampCount % 100 == 1) {
+				System.out.println("Current count is [" + PogChampCount + "]. My count is [" + myPogChamps + "]");
 				writePogChampsToFile();
 			}
 		}
@@ -95,7 +105,7 @@ class PogChampBot extends PircBot {
 		}
 		
 		
-		return (counter > 2);
+		return (counter > 0);
 	}
 	
 	public void checkActivity() {
@@ -136,10 +146,12 @@ class PogChampBot extends PircBot {
 	
 	boolean writePogChampsToFile() {
 		File file = new File("data/PogChampCount.txt");
-		FileWriter fw;
+		BufferedWriter fw;
 		try {
-			fw = new FileWriter(file.getAbsoluteFile());
-			fw.write(Integer.toString(PogChampCount));
+			fw = new BufferedWriter(new FileWriter(file.getAbsoluteFile()));
+			fw.write(Integer.toString(PogChampCount) + "\n");
+			fw.newLine();
+			fw.write(Integer.toString(myPogChamps));
 			System.out.println("Wrote to file successfully!");
 			fw.close();
 		} catch (IOException e) {
@@ -156,16 +168,17 @@ class PogChampBot extends PircBot {
 		BufferedReader reader = new BufferedReader(new FileReader(file));
 
 		PogChampCount = Integer.parseInt(reader.readLine());
+		reader.readLine();
+		myPogChamps = Integer.parseInt(reader.readLine());
 		reader.close();
 		
-		System.out.println("PogChamps loaded! Current count is [" + PogChampCount + "]");
+		System.out.println("PogChamps loaded! Current count is [" + PogChampCount + "]. My count is [" + myPogChamps + "]");
 		
 	}
 	
 	protected void onDisconnect() {
 		
 		writePogChampsToFile();
-		
 		
 	}
 
